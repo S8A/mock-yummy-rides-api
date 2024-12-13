d59bf2212e96ecb3370af8ffe5c132cccd6d3c80
S8A <samuelochoap@gmail.com>
2024-12-13 16:38:12 -0400

    Add MongoDB & Beanie ODM to project

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     API_KEY: str
     WEBHOOK_URL: str
-
+    MONGODB_URL: str = "mongodb://mongodb:27017"
+    MONGODB_DB_NAME: str = "yummy_rides"
 
 settings = Settings()
